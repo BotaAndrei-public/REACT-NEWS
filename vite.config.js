e196dc51afc12fec4d5dfcdbe148cdea6958a6d7
Andrei Bota <andrei.bota@example.com>
2025-04-02 04:31:05 +0300

    Fix: Adăugat calea relativă pentru vite.svg și copierea în src

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/repository-name/", // Înlocuiește cu numele repository-ului tău
+  base: "/REACT-NEWS/", // Numele repository-ului tău
 });
